f5ee42d5059c4908becca67b96fb9e4006c20a8f
nitishtrivedi <nitish_trivedi0194@yahoo.com>
2022-11-29 19:23:47 +0530

    Order Router Pointed to RENDER

--- routers/orders-router.js
+++ routers/orders-router.js
@@ -1,27 +1,27 @@
 //-------ROUTES------
 
 //Import order Model from models
-const { Order } = require('../models/order-model.js');
+const { Order } = require("../models/order-model.js");
 //Require the ExpressJS library in the order Route
-const express = require('express');
+const express = require("express");
 //Import orderItem schema from orderItems-model.js
-const { OrderItem } = require('../models/orderItem-model');
+const { OrderItem } = require("../models/orderItem-model");
 //Import Product Model
-const { Product } = require('../models/product-model');
+const { Product } = require("../models/product-model");
 //Import MONGOOSE. Run 'npm install mongoose' in terminal. Used for DB interaction with MongoDB
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Initialize the route
 const router = express.Router();
 //Import STRIPE For payments //Lec215 22/9/22
-const stripe = require('stripe')(
-  'sk_test_51LkYoBSBgojFKKnRW4S8XCSgr8YwwND7ZMjIvdo3XgOYMmFKHAXEbaaGIQOS6lofjMEJvggshR2Jr4eIutj5ArlS00sdTRRHqK'
+const stripe = require("stripe")(
+  "sk_test_51LkYoBSBgojFKKnRW4S8XCSgr8YwwND7ZMjIvdo3XgOYMmFKHAXEbaaGIQOS6lofjMEJvggshR2Jr4eIutj5ArlS00sdTRRHqK"
 );
 
 //Creating routes. Default API route is http://localhost:300/api/v1/
 router.get(`/`, async (req, res) => {
   //Get request finds order, populates user details, and sorts orders from newest to oldest
   const ordersList = await Order.find()
-    .populate('user', 'name email')
+    .populate("user", "name email")
     .sort({ dateOrdered: -1 });
 
   if (!ordersList) {
@@ -34,12 +34,12 @@ router.get(`/`, async (req, res) => {
 router.get(`/:id`, async (req, res) => {
   //Get request finds order by ID and populates user details who created the order
   const orders = await Order.findById(req.params.id)
-    .populate('user', 'name email')
+    .populate("user", "name email")
     .populate({
-      path: 'orderItems',
+      path: "orderItems",
       populate: {
-        path: 'product',
-        populate: 'category',
+        path: "product",
+        populate: "category",
       },
     });
 
@@ -51,7 +51,7 @@ router.get(`/:id`, async (req, res) => {
 
 //Create a POST Request
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   //Reference for orderItems, by importing orderItem-schema from models.
   const orderItemsIds = Promise.all(
     req.body.orderItems.map(async (orderItem) => {
@@ -70,8 +70,8 @@ router.post('/', async (req, res) => {
   const totalPrices = await Promise.all(
     orderItemsIdsResolved.map(async (orderItemId) => {
       const orderItem = await OrderItem.findById(orderItemId).populate(
-        'product',
-        'price'
+        "product",
+        "price"
       );
       const totalPrice = orderItem.product.price * orderItem.quantity;
       return totalPrice;
@@ -95,20 +95,20 @@ router.post('/', async (req, res) => {
   });
   order = await order.save();
 
-  if (!order) return res.status(400).send('the order cannot be created!');
+  if (!order) return res.status(400).send("the order cannot be created!");
 
   res.send(order);
 });
 
 /////////// CHECKOUT API FOR PAYMENTS. Created on 22/9/22 Lecture Number 215. ////////////////
 
-router.post('/create-checkout-session', async (req, res) => {
+router.post("/create-checkout-session", async (req, res) => {
   const orderItems = req.body;
 
   if (!orderItems) {
     return res
       .status(400)
-      .send('CheckOut Session cannot be created. Check Order Items orderItems');
+      .send("CheckOut Session cannot be created. Check Order Items orderItems");
   }
 
   const lineItems = await Promise.all(
@@ -116,11 +116,11 @@ router.post('/create-checkout-session', async (req, res) => {
       const product = await Product.findById(orderItem.product);
       return {
         price_data: {
-          currency: 'inr',
+          currency: "inr",
           product_data: {
             name: product.name,
           },
-          unit_amount: product.price + '00', //Used the '00' for getting the right price while on payment screen.
+          unit_amount: product.price + "00", //Used the '00' for getting the right price while on payment screen.
         },
         quantity: orderItem.quantity,
       };
@@ -128,17 +128,17 @@ router.post('/create-checkout-session', async (req, res) => {
   );
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    payment_method_types: ["card"],
     line_items: lineItems,
-    mode: 'payment',
-    success_url: 'https://pinkbits.herokuapp.com/thank-you', //Change this to http://localhost:1000/thank-you when running on localhost
-    cancel_url: 'https://pinkbits.herokuapp.com/failure', //Change this to http://localhost:1000/failure when running on localhost
+    mode: "payment",
+    success_url: "https://pinkbits.onrender.com/thank-you", //Change this to http://localhost:1000/thank-you when running on localhost
+    cancel_url: "https://pinkbits.onrender.com/failure", //Change this to http://localhost:1000/failure when running on localhost
   });
   res.json({ id: session.id });
 });
 
 //Update an Order
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   const order = await Order.findByIdAndUpdate(
     req.params.id,
     {
@@ -152,14 +152,14 @@ router.put('/:id', async (req, res) => {
   if (!order) {
     return res
       .status(500)
-      .send('The order cannot be updated. Please check ID and try again');
+      .send("The order cannot be updated. Please check ID and try again");
   }
 
   res.status(200).send(order);
 });
 
 //Delete an order and underlying orderItem
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const order = await Order.findByIdAndDelete(req.params.id)
     .then(async (order) => {
       if (order) {
@@ -170,12 +170,12 @@ router.delete('/:id', async (req, res) => {
         return res
           .status(200)
           .send(
-            'The order was successfully deleted. Order Items in the deleted order has been deleted as well'
+            "The order was successfully deleted. Order Items in the deleted order has been deleted as well"
           );
       } else {
         return res.status(404).json({
           success: false,
-          message: 'The order was not found. Check Order ID',
+          message: "The order was not found. Check Order ID",
         });
       }
     })
@@ -185,22 +185,22 @@ router.delete('/:id', async (req, res) => {
 });
 
 //GET total sales data for the ADMIN
-router.get('/get/totalsales', async (req, res) => {
+router.get("/get/totalsales", async (req, res) => {
   const totalSales = await Order.aggregate([
-    { $group: { _id: null, totalSales: { $sum: '$totalPrice' } } }, // Use $totalPrice instead of totalPrice
+    { $group: { _id: null, totalSales: { $sum: "$totalPrice" } } }, // Use $totalPrice instead of totalPrice
   ]);
 
   if (!totalSales) {
-    return res.status(400).send('The sales cannot be generated');
+    return res.status(400).send("The sales cannot be generated");
   }
   res.send({
-    message: 'The total sales are as shown below',
+    message: "The total sales are as shown below",
     totalSales: totalSales.pop().totalSales,
   });
 });
 
 //GET the count of total orders in the order bucket
-router.get('/get/count', async (req, res) => {
+router.get("/get/count", async (req, res) => {
   const orderCount = await Order.countDocuments(); // Remove (count) => count from Order.countDocuments()
 
   if (!orderCount) {
@@ -213,22 +213,22 @@ router.get('/get/count', async (req, res) => {
 });
 
 //GET orders based on the users who placed the orders. Get order history for a specific user
-router.get('/get/userorders/:userid', async (req, res) => {
+router.get("/get/userorders/:userid", async (req, res) => {
   //GET userTotalOrders
   const userTotalOrders = await Order.countDocuments();
   if (!userTotalOrders) {
     return res
       .send(500)
-      .json({ success: false, message: 'This user has no orders placed' });
+      .json({ success: false, message: "This user has no orders placed" });
   }
 
   //Get userOrderList
   const userOrderList = await Order.find({ user: req.params.userid })
     .populate({
-      path: 'orderItems',
+      path: "orderItems",
       populate: {
-        path: 'product',
-        populate: 'category',
+        path: "product",
+        populate: "category",
       },
     })
     .sort({ dateOrdered: -1 });
